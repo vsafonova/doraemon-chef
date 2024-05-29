39a59b9a986ef573bbe06b2486acad952c767402
Kseniia Tikhonova <tikhonovakb@gmail.com>
2024-05-29 13:21:20 +0200

    init

---
+++ src/index.ts
@@ -0,0 +1,35 @@
+import {
+  ViewerApp,
+  AssetManagerPlugin,
+//   GBufferPlugin,
+//   ProgressivePlugin,
+//   TonemapPlugin,
+//   SSRPlugin,
+//   SSAOPlugin,
+//   mobileAndTabletCheck,
+//   BloomPlugin,
+//   Vector3,
+//   GammaCorrectionPlugin,
+//   MeshBasicMaterial2,
+//   Color,
+//   AssetImporter,
+  addBasePlugins,
+} from 'webgi';
+import './styles.css';
+
+async function setupViewer() {
+  const viewer = new ViewerApp({
+    canvas: document.getElementById('webgi-canvas') as HTMLCanvasElement,
+    useRgbm: true,
+  });
+
+  const manager = await viewer.addPlugin(AssetManagerPlugin);
+
+  await addBasePlugins(viewer);
+
+  viewer.renderer.refreshPipeline();
+
+  await manager.addFromPath('./assets/drill3.glb');
+}
+
+setupViewer();
