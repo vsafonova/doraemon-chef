dafdb806af94ee8e883b6bc7dda9c6490df681e8
viktoriia <viktoriia.safonova@hyperisland.se>
2024-05-31 23:15:08 +0200

    Prettier formattig

--- src/chefAnimation.js
+++ src/chefAnimation.js
@@ -1,24 +1,26 @@
-let windowHeight =  Math.max(document.documentElement.clientHeight, window.innerHeight || 0),
-    lastTop;
+let windowHeight = Math.max(
+    document.documentElement.clientHeight,
+    window.innerHeight || 0
+  ),
+  lastTop;
 
-window.addEventListener('scroll', function(event) {
-  var animation = document.getElementById('chefMotion'),
-      top = animation.getBoundingClientRect().top,
-      offset = top - windowHeight;
-      
-      if (offset > 0) {
-      	animation.classList.remove('animation');
-      	return;
-      }
-      
-      if (top < windowHeight / 2 && top > lastTop) {
-      	animation.classList.remove('animation');
-      }
-      
-      if (animation.className.indexOf('animation') === -1 && top < lastTop) {
-      	animation.classList.add('animation');
-      }
-      
-      lastTop = top;
-     
+window.addEventListener("scroll", function (event) {
+  var animation = document.getElementById("chefMotion"),
+    top = animation.getBoundingClientRect().top,
+    offset = top - windowHeight;
+
+  if (offset > 0) {
+    animation.classList.remove("animation");
+    return;
+  }
+
+  if (top < windowHeight / 2 && top > lastTop) {
+    animation.classList.remove("animation");
+  }
+
+  if (animation.className.indexOf("animation") === -1 && top < lastTop) {
+    animation.classList.add("animation");
+  }
+
+  lastTop = top;
 });
