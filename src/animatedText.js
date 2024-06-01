0480beea0a82ae9597ac8fc6a971bc6a71d05fcb
elen-oz <ozerova.lenka@gmail.com>
2024-06-02 00:27:07 +0200

    feat: change text animation on 4th section

--- src/animatedText.js
+++ src/animatedText.js
@@ -9,9 +9,21 @@ const textEl = new SplitType("#text");
 gsap.to(".char", {
   scrollTrigger: {
     trigger: "#text",
-    start: "top bottom",
+    start: "top center",
   },
   y: 0,
+  stagger: 0.05,
+  delay: 0.2,
+  duration: 0.1,
+});
+
+gsap.to(".char", {
+  scrollTrigger: {
+    trigger: "#text",
+    start: "bottom center",
+    scrub: true,
+  },
+  x: -400,
   stagger: 0.05,
   delay: 0.2,
   duration: 0.1,
