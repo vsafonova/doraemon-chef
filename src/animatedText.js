6008f6c94b74a260cc0e179bcb8c3da859577c38
elen-oz <ozerova.lenka@gmail.com>
2024-06-02 19:32:15 +0200

    feat: add test animation with dialog buble

--- src/animatedText.js
+++ src/animatedText.js
@@ -2,8 +2,41 @@ import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import SplitType from "split-type";
 
-gsap.registerPlugin(ScrollTrigger);
+import Lenis from "lenis";
 
+gsap.registerPlugin(ScrollTrigger);
+// ------------------------------------------------------
+// https://www.youtube.com/watch?v=PrQeeUt49f4
+
+let tl = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".animated-text",
+    start: "top 80%",
+    end: "bottom 60%",
+    scrub: true,
+    markers: true,
+  },
+});
+
+tl.to(".animated-text", {
+  x: 1300,
+});
+
+// smooth scroll
+const lenis = new Lenis();
+
+lenis.on("scroll", (e) => {
+  console.log(e);
+});
+
+function raf(time) {
+  lenis.raf(time);
+  requestAnimationFrame(raf);
+}
+
+requestAnimationFrame(raf);
+
+// ---------------------------------------------------------
 const textEl = new SplitType("#text");
 
 gsap.to(".char", {
