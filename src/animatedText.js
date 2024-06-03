a53712fc605abc7f99a90a287aa362c6ec7555e6
elen-oz <ozerova.lenka@gmail.com>
2024-06-03 11:53:49 +0200

    fix: text-animation works better

--- src/animatedText.js
+++ src/animatedText.js
@@ -41,14 +41,36 @@ requestAnimationFrame(raf);
 // ---------------------------------------------------------
 const textEl = new SplitType("#text");
 
+// gsap.to(".char", {
+//   scrollTrigger: {
+//     trigger: "#text",
+//     start: "top 80%",
+//   },
+//   x: 0,
+//   stagger: 0.05,
+//   delay: 0.2,
+//   duration: 0.1,
+// });
+
+// gsap.to(".char", {
+//   scrollTrigger: {
+//     trigger: "#text",
+//     start: "bottom 30%",
+//     scrub: true,
+//     markers: true,
+//   },
+//   x: 400,
+//   stagger: 0.03,
+//   duration: 0.1,
+// });
+
 gsap.to(".char", {
   scrollTrigger: {
     trigger: "#text",
-    // start: "top center",
-    start: "top 80%",
+    start: "top center",
+    // start: "top 80%",
   },
-  // y: 0,
-  x: 0,
+  y: 0,
   stagger: 0.05,
   delay: 0.2,
   duration: 0.1,
@@ -57,16 +79,12 @@ gsap.to(".char", {
 gsap.to(".char", {
   scrollTrigger: {
     trigger: "#text",
-    // start: "bottom center",
     start: "bottom 30%",
-    // end: "bottom 10%",
     scrub: true,
     markers: true,
   },
-  // x: -400,
   x: 400,
-  stagger: 0.05,
-  // delay: 0.2,
+  stagger: 0.03,
   duration: 0.1,
 });
 
